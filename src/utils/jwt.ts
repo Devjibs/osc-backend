30dd44062b5c44a53ccb33948609ee483ef5a21b
Habeeb Ajide <ajidejibola@yahoo.com>
2025-03-03 06:51:17 +0000

    update expiresIn

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,2 +1,2 @@
 export const tokenExpiration = () =>
-  process.env.TOKEN_EXPIRES_IN ? Number(process.env.TOKEN_EXPIRES_IN) : '1h';
+  process.env.TOKEN_EXPIRES_IN ? Number(process.env.TOKEN_EXPIRES_IN) : 3600;
